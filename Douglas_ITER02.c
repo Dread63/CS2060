7c5073e501651b2f07fa4e7bd8d870434eeb91c7
Josh Douglas <jmdcolorado11@protonmail.com>
2024-03-24 18:59:03 -0600

    loginAdmin() Function Completed

--- Douglas_ITER02.c
+++ Douglas_ITER02.c
@@ -8,8 +8,13 @@
 #define INVALID_INPUT 1;
 
 bool scanDouble(const char *buffer, double *output);
+bool loginAdmin (const char *correctID, const char *correctPassword, unsigned int maxAttempts);
+void fgetsRemoveNewLine (char *str, unsigned int maxSize);
 
 int main(void) {
+
+    const char correctID[80] = "Josh";
+    const char correctPassword[80] = "Pizza2020";
 
     char testString[80] = "3.14159";
 
@@ -18,6 +23,16 @@ int main(void) {
     bool validScan = scanDouble(testString, &input);
 
     printf("%f", input);
+
+    bool validLogin = loginAdmin(correctID, correctPassword, 3);
+
+    if (validLogin == true) {
+        puts("Successful Login");
+    }
+
+    if (validLogin == false) {
+        puts("Failed Login");
+    }
 }
 
 bool scanDouble (const char *buffer, double *output) {
@@ -44,4 +59,54 @@ bool scanDouble (const char *buffer, double *output) {
     }
 
     return isValid;
+}
+
+bool loginAdmin (const char *correctID, const char *correctPassword, unsigned int maxAttempts) {
+
+    unsigned int attempts = 1;
+    bool validLogin = false;
+    char enteredID[80];
+    char enteredPass[80];
+
+    while (attempts <= maxAttempts && !validLogin) {
+
+        puts("\nEnter user login ID followed by the enter key");
+
+        fgetsRemoveNewLine(enteredID, 80);
+
+        puts("\nEnter passcode followed by the enter key");
+
+        fgetsRemoveNewLine(enteredPass, 80);
+
+        if (strcmp(enteredID, correctID) == 0) {
+
+            if (strcmp(enteredPass, correctPassword) == 0) {
+                validLogin = true;
+            }
+
+            else {
+                puts("\nIncorrect Password, Please Try Again");
+            }
+        }
+
+        else {
+            puts("Incorrect UserID, Please Try Again");
+        }
+
+        attempts++;
+    }
+
+    return validLogin;
+}
+
+void fgetsRemoveNewLine (char *str, unsigned int maxSize) {
+
+    fgets(str, maxSize, stdin);
+
+    int strLength = strlen(str);
+
+    if (strLength > 0 && str[strLength - 1] == '\n') {
+
+        str[strLength - 1] = '\0';
+    }
 }
