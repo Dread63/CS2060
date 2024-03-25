63f96f0d852652b6db2ab377dfe54943e7a29b8c
J <123434357+Dread63@users.noreply.github.com>
2024-03-24 19:19:46 -0600

    Fixed Function Prototyes

--- Douglas_ITER02.c
+++ Douglas_ITER02.c
@@ -11,7 +11,7 @@ bool scanDouble(const char *buffer, double *output);
 bool loginAdmin (const char *correctID, const char *correctPassword, unsigned int maxAttempts);
 void fgetsRemoveNewLine (char *str, unsigned int maxSize);
 size_t strlen(const char* str);
-int strcmp(first_str, second_str );
+int strcmp(char first_str, char second_str );
 
 int main(void) {
 
